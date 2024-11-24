12b06ed2890fdde4ef4667e2ae40463b3e7f625d
anruichan <anrui2032@gmail.com>
2024-11-25 01:39:14 +0800

    r7plus: Move out lineage overlays from AOSP overlay
    Change-Id: Ic22f971a4570827596d6bd3a50d2f4499d69d983

--- device.mk
+++ device.mk
@@ -16,7 +16,9 @@
 #
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l_mr1.mk)
 
